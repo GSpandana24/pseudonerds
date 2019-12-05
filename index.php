986f8696b35ce2746644f8f9b5107372c6dfc6dd
ShwethaP24 <shwethz.14@gmail.com>
2019-12-05 18:47:22 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 
-mysqli_connect("mysql-gamification.inmbzp8022.in.dst.ibm.com","xxuser","welcome1","sampledb");
+mysqli_connect("9.193.199.22","xxuser","welcome1","sampledb");
 if(mysqli_connect_error())
 {
 echo "error1";
