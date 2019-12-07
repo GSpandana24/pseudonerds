78767776bd47c73bdd9c20a73ee9d6048eb16a6d
Shwetha <shwepati@in.ibm.com>
2019-12-07 12:52:18 +0530

    added package.json, updated all the packages in nodemodule folders, changed .less to .css extension. added a new file index.js to run the static files from nodejs

--- PseudoNerds/index.js
+++ PseudoNerds/index.js
@@ -1,5 +1,22 @@
-var express = require('express');
-var app = express(); // serves files from the root directory
-app.use(express.static('./'));app.listen(8080, function () {    
-  console.log('Listening at http://localhost:8080');  
+const express = require('express');
+const app = express();
+const path = require('path');
+const router = express.Router();
+
+router.get('/',function(req,res){
+  res.sendFile(path.join(__dirname+'/home.html'));
+  //__dirname : It will resolve to your project folder.
 });
+
+//add the router
+app.use(express.static(__dirname + '/'));
+//Store all HTML files in view folder.
+app.use(express.static(__dirname + '/scripts'));
+//Store all JS and CSS in Scripts folder.
+app.use(express.static(__dirname + '/stylesheets'));
+//Store all  CSS in stylesheets folder.
+
+app.use('/', router);
+app.listen(process.env.port || 3000);
+
+console.log('Running at Port 3000');
