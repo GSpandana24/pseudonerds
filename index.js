fa879ccfd7deaaeed268c767d025e33547a4143b
ShwethaP24 <shwethz.14@gmail.com>
2019-12-07 13:32:32 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const router = express.Router();
+
+router.get('/',function(req,res){
+  res.sendFile(path.join(__dirname+'/home.html'));
+  //__dirname : It will resolve to your project folder.
+});
+
+//add the router
+app.use(express.static(__dirname + '/'));
+//Store all HTML files in view folder.
+app.use(express.static(__dirname + '/scripts'));
+//Store all JS and CSS in Scripts folder.
+app.use(express.static(__dirname + '/stylesheets'));
+//Store all  CSS in stylesheets folder.
+
+var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
+    ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
+
+app.use('/', router);
+app.listen(port, ip);
+console.log('Server running on http://%s:%s', ip, port);
+
+console.log('Running at Port 3000');
